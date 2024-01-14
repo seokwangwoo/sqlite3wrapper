ace576a68a601598c684b72f44c9dd3809b8c374
Kwangwoo Seo <seokwangwoo@gmail.com>
2024-01-14 16:09:24 +0900

    Fix sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
